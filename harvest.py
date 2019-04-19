cf68d211e01539970675a9f0c5200eb3caf60130
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-18 17:01:39 -0700

    finally finished part 1 -_-

--- harvest.py
+++ harvest.py
@@ -10,7 +10,7 @@ class MelonType():
         self.first_harvest = first_harvest
         self.color = color
         self.is_seedless = is_seedless
-        self.is_bestseller = is_bestseller
+        self.is_bestseller =is_bestseller
         self.name = name 
         self.pairings = []
 
@@ -28,8 +28,8 @@ def make_melon_types():
     """Returns a list of current melon types."""
 
     all_melon_types = []
-
-
+    crenshaw = MelonType('cren', 1996, 'green', 'has seeds', 'not_bestseller', 'Crenshaw')
+    yellow_watermelon = MelonType('yw', 2013, 'yellow', 'has seeds', 'bestseller', 'Yellow Watermelon')
     muskmelon = MelonType('musk', 1998, 'green',  'seedless', 'bestseller', 'Muskmelon')
     casaba = MelonType('cas', 2003, 'orange', 'has seeds', 'not bestseller', 'Casaba')
     crenshaw = MelonType('cren', 1996, 'green', 'has seeds', 'not_bestseller', 'Crenshaw')
@@ -49,6 +49,8 @@ def make_melon_types():
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
+    for melon in melon_types:
+        print(f'{melon.name} pairs with {melon.pairings}')
 
     # Fill in the rest
 
@@ -57,6 +59,25 @@ def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
     # Fill in the rest
+    melon_dict = {}
+    for melon in melon_types:
+        melon_dict[melon.name] = melon.code
+    print(melon_dict)
+melon_types = []
+
+
+
+crenshaw = MelonType('cren', 1996, 'green', 'has seeds', 'not_bestseller', 'Crenshaw')
+yellow_watermelon = MelonType('yw', 2013, 'yellow', 'has seeds', 'bestseller', 'Yellow Watermelon')
+muskmelon = MelonType('musk', 1998, 'green',  'seedless', 'bestseller', 'Muskmelon')
+casaba = MelonType('cas', 2003, 'orange', 'has seeds', 'not bestseller', 'Casaba')
+melon_types.append(crenshaw)
+melon_types.append(yellow_watermelon)
+melon_types.append(muskmelon)
+melon_types.append(casaba)
+
+
+make_melon_type_lookup(melon_types)
 
 ############
 # Part 2   #
@@ -65,7 +86,8 @@ def make_melon_type_lookup(melon_types):
 class Melon(object):
     """A melon in a melon harvest."""
 
-    # Fill in the rest
+    # def __init__:
+
     # Needs __init__ and is_sellable methods
 
 def make_melons(melon_types):
@@ -78,5 +100,14 @@ def get_sellability_report(melons):
 
     # Fill in the rest 
 
+muskmelon = MelonType('musk', 1998, 'green',  'seedless', 'bestseller', 'Muskmelon')
+casaba = MelonType('cas', 2003, 'orange', 'has seeds', 'not bestseller', 'Casaba')
+crenshaw = MelonType('cren', 1996, 'green', 'has seeds', 'not_bestseller', 'Crenshaw')
+yellow_watermelon = MelonType('yw', 2013, 'yellow', 'has seeds', 'bestseller', 'Yellow Watermelon')
 
 
+melon_types = [muskmelon, casaba, crenshaw, yellow_watermelon]
+print(print_pairing_info(melon_types))
+
+make_melon_types()
+print_pairing_info(melon_types)
